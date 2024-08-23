6b9d39da845d21601c7ab9d317d15d4b24824daa
kangkigs <dlflq11@gmail.com>
2024-08-23 10:14:59 +0900

    first

---
+++ src/main/java/org/example/jenkinstest/TestController.java
@@ -0,0 +1,15 @@
+package org.example.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/test01")
+    public String test01(){
+        return "test01";
+    }
+}
